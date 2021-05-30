a8ccb1231991397998b00d32459bfd0acddc36fe
Fatsfish <tuankhai512@gmail.com>
2021-05-30 10:11:25 +0700

    first commit

---
+++ Ass01Solution/BusinessObject/MemberObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BusinessObject
+{
+    public class MemberObject
+    {
+        public int MemberID { get; set; }
+        public string MemberName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+    }
+}
